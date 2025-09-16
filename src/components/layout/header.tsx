0645b050946c3e80ce66b75e9ea334cf3de51674
Maniksa Dhongadi <maniksadhongadi@gmail.com>
2025-09-16 16:31:59 +0000

    Make the name of the website bigger

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -5,7 +5,7 @@ export function Header() {
     <header className="py-6 px-4 md:px-6 border-b">
       <div className="container mx-auto flex items-center gap-4">
         <Logo className="h-16 w-16 text-primary" />
-        <h1 className="text-2xl font-bold tracking-tight text-foreground font-headline">
+        <h1 className="text-4xl font-bold tracking-tight text-foreground font-headline">
           SanataniShop
         </h1>
       </div>
