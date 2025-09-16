6e1b39a222e144e7cfe7266a1bccb4f8df462ad3
Maniksa Dhongadi <maniksadhongadi@gmail.com>
2025-09-16 16:30:50 +0000

    Make the logo 2 x bigger

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <header className="py-6 px-4 md:px-6 border-b">
       <div className="container mx-auto flex items-center gap-4">
-        <Logo className="h-8 w-8 text-primary" />
+        <Logo className="h-16 w-16 text-primary" />
         <h1 className="text-2xl font-bold tracking-tight text-foreground font-headline">
           SanataniShop
         </h1>
