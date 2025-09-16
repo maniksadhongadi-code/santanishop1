0ac7b12fc4cbdff6c5903ed6a99e17c8e5fff557
Maniksa Dhongadi <maniksadhongadi@gmail.com>
2025-09-16 16:30:01 +0000

    https://iili.io/KRCtUdv.md.jpg "this is my logo of the website add this"

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,18 +1,17 @@
-import type { SVGProps } from 'react';
+import Image from 'next/image';
+import type { HTMLAttributes } from 'react';
+import { cn } from '@/lib/utils';
 
-export function Logo(props: SVGProps<SVGSVGElement>) {
+export function Logo(props: HTMLAttributes<HTMLDivElement>) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
-    </svg>
+    <div {...props}>
+      <Image
+        src="https://iili.io/KRCtUdv.md.jpg"
+        alt="SanataniShop Logo"
+        width={50}
+        height={50}
+        className={cn('h-full w-full', props.className)}
+      />
+    </div>
   );
 }
